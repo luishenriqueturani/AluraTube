8a302b4ae3a749b9a2b5d1e8359e24e9e17df9ef
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2022-11-12 15:39:52 -0300

    Update index.js
    Corrigido o warning da key

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ function Timeline({searchValue, ...props}){
                 playlistNames.map(function(playlistName){
                     const videos = props.playlists[playlistName]
                     return (
-                        <section>
+                        <section key={playlistName}>
                             <h2>{playlistName}</h2>
                             <div>
                                 {
@@ -84,7 +84,7 @@ function Timeline({searchValue, ...props}){
                                         return video.title.toLowerCase().includes(searchValue.toLowerCase())
                                     }).map((video) =>{
                                         return (
-                                            <a href={video.url}>
+                                            <a key={video.url} href={video.url}>
                                                 <img src={video.thumb}/>
                                                 <span>
                                                     {video.title}
